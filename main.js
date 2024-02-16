164b986b78fabfe1d7163db8c7f520af0d25dc99
Alipa <alipakkr24@gmail.com>
2024-02-16 20:52:34 +0530

    done

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
 // preloader script ends here.........
 
 // switch for setting
-function settingtoggle(){
+function settingtoggle() {
   document.getElementById("setting-container").classList.toggle('settingactivate');
   document.getElementById("visualmodetogglebuttoncontainer").classList.toggle('visualmodeshow');
   document.getElementById("soundtogglebuttoncontainer").classList.toggle('soundmodeshow');
@@ -17,114 +17,111 @@ function settingtoggle(){
 function playpause() {
   if (document.getElementById('switchforsound').checked == false) {
     audio.pause();
-   }
-
- else{
-     audio.play();
- }
   }
-
-  function visualmode(){
-    document.body.classList.toggle('light-mode');
-    document.getElementById('labelforsound').classList.toggle('invertsoundlabel');
-    document.querySelector('.about-heading-article').classList.toggle('heading-article-light');
-    document.querySelector('.aboutHeadingP').classList.toggle('heading-article-light');
-
-    document.querySelector('.skills-heading-article').classList.toggle('heading-article-light');
-    document.querySelector('.skillsHeadingP').classList.toggle('heading-article-light');
-
-    document.querySelector('.projects-heading-article').classList.toggle('heading-article-light');
-    document.querySelector('.projectsHeadingP').classList.toggle('heading-article-light');
-   
-    document.querySelector('.frontend-dev-heading').classList.toggle('heading-article-light');
-    document.querySelector('.designing-heading').classList.toggle('heading-article-light');
-    document.querySelector('.languages-heading').classList.toggle('heading-article-light');
-
-    document.getElementById('html').classList.toggle('language-gradient-class');
-    document.getElementById('css').classList.toggle('language-gradient-class');
-    document.getElementById('bootstrap').classList.toggle('language-gradient-class');
-    document.getElementById('react').classList.toggle('language-gradient-class');
-    document.getElementById('js').classList.toggle('language-gradient-class');
-    document.getElementById('ap').classList.toggle('language-gradient-class');
-    document.getElementById('canva').classList.toggle('language-gradient-class');
-    document.getElementById('ai').classList.toggle('language-gradient-class');
-    document.getElementById('c').classList.toggle('language-gradient-class');
-    document.getElementById('cpp').classList.toggle('language-gradient-class');
-    // document.getElementById('project-box1').classList.toggle('language-gradient-class');
-    // document.getElementById('project-box2').classList.toggle('language-gradient-class');
-    let projectBox = document.querySelectorAll('.project-box');
-    projectBox.forEach(Box => {
-        Box.classList.toggle('language-gradient-class');
-    })
-
+  else {
+    audio.play();
   }
+}
+function visualmode() {
+  document.body.classList.toggle('light-mode');
+  document.getElementById('labelforsound').classList.toggle('invertsoundlabel');
+  document.querySelector('.about-heading-article').classList.toggle('heading-article-light');
+  document.querySelector('.aboutHeadingP').classList.toggle('heading-article-light');
+
+  document.querySelector('.skills-heading-article').classList.toggle('heading-article-light');
+  document.querySelector('.skillsHeadingP').classList.toggle('heading-article-light');
+
+  document.querySelector('.projects-heading-article').classList.toggle('heading-article-light');
+  document.querySelector('.projectsHeadingP').classList.toggle('heading-article-light');
+
+  document.querySelector('.frontend-dev-heading').classList.toggle('heading-article-light');
+  document.querySelector('.designing-heading').classList.toggle('heading-article-light');
+  document.querySelector('.languages-heading').classList.toggle('heading-article-light');
+
+  document.getElementById('html').classList.toggle('language-gradient-class');
+  document.getElementById('css').classList.toggle('language-gradient-class');
+  document.getElementById('bootstrap').classList.toggle('language-gradient-class');
+  document.getElementById('react').classList.toggle('language-gradient-class');
+  document.getElementById('js').classList.toggle('language-gradient-class');
+  document.getElementById('ap').classList.toggle('language-gradient-class');
+  document.getElementById('canva').classList.toggle('language-gradient-class');
+  document.getElementById('ai').classList.toggle('language-gradient-class');
+  document.getElementById('c').classList.toggle('language-gradient-class');
+  document.getElementById('cpp').classList.toggle('language-gradient-class');
+  // document.getElementById('project-box1').classList.toggle('language-gradient-class');
+  // document.getElementById('project-box2').classList.toggle('language-gradient-class');
+  let projectBox = document.querySelectorAll('.project-box');
+  projectBox.forEach(Box => {
+    Box.classList.toggle('language-gradient-class');
+  })
+
+}
 let emptyArea = document.getElementById("emptyarea");
- let mobileTogglemenu = document.getElementById("mobiletogglemenu");
+let mobileTogglemenu = document.getElementById("mobiletogglemenu");
 // toggle menu by clicking on hamburger
 function hamburgerMenu() {
-    document.body.classList.toggle("stopscrolling");
- document.getElementById("mobiletogglemenu").classList.toggle("show-toggle-menu");
-document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
-document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
-document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
+  document.body.classList.toggle("stopscrolling");
+  document.getElementById("mobiletogglemenu").classList.toggle("show-toggle-menu");
+  document.getElementById("burger-bar1").classList.toggle("hamburger-animation1");
+  document.getElementById("burger-bar2").classList.toggle("hamburger-animation2");
+  document.getElementById("burger-bar3").classList.toggle("hamburger-animation3");
 }
 // close mobile toggle menu by clicking on LI
-function hidemenubyli(){
-document.body.classList.toggle("stopscrolling");
-document.getElementById("mobiletogglemenu").classList.remove("show-toggle-menu");
-document.getElementById("burger-bar1").classList.remove("hamburger-animation1");
-document.getElementById("burger-bar2").classList.remove("hamburger-animation2");
-document.getElementById("burger-bar3").classList.remove("hamburger-animation3");
+function hidemenubyli() {
+  document.body.classList.toggle("stopscrolling");
+  document.getElementById("mobiletogglemenu").classList.remove("show-toggle-menu");
+  document.getElementById("burger-bar1").classList.remove("hamburger-animation1");
+  document.getElementById("burger-bar2").classList.remove("hamburger-animation2");
+  document.getElementById("burger-bar3").classList.remove("hamburger-animation3");
 }
 
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('.navbar .navbar-tabs .navbar-tabs-ul a li');
 const mobilenavLi = document.querySelectorAll('.mobiletogglemenu .mobile-navbar-tabs-ul a li');
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
   let current = "";
   sections.forEach(section => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if(pageYOffset >= (sectionTop - 500)){
+    if (pageYOffset >= (sectionTop - 500)) {
       current = section.getAttribute('id');
     }
   })
 
-  mobilenavLi.forEach( li => {
+  mobilenavLi.forEach(li => {
     li.classList.remove('activeThismobiletab');
-    if(li.classList.contains(current)){
+    if (li.classList.contains(current)) {
       li.classList.add('activeThismobiletab')
     }
   })
-navLi.forEach( li => {
-  li.classList.remove('activeThistab');
-  if(li.classList.contains(current)){
-    li.classList.add('activeThistab')
-  }
-})
+  navLi.forEach(li => {
+    li.classList.remove('activeThistab');
+    if (li.classList.contains(current)) {
+      li.classList.add('activeThistab')
+    }
+  })
 })
 console.log('%c Designed and Developed by Alipa', 'background-image: linear-gradient(90deg,#8000ff,#6bc5f8); color: white;font-weight:900;font-size:1rem; padding:20px;');
 
 
 
 let mybutton = document.getElementById("backtotopbutton");
-window.onscroll = function(){
+window.onscroll = function () {
   scrollFunction()
 };
 
-function scrollFunction(){
-  if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400)
-  {
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     mybutton.style.display = "block";
   }
-   else{
-      mybutton.style.display = "none";
-     
-      }
+  else {
+    mybutton.style.display = "none";
+
+  }
 }
 
-function scrolltoTopfunction(){
+function scrolltoTopfunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
@@ -132,9 +129,9 @@ function scrolltoTopfunction(){
 //   e.preventDefault();
 // }, false);
 
-document.addEventListener("contextmenu", function(e){
+document.addEventListener("contextmenu", function (e) {
   if (e.target.nodeName === "IMG") {
-      e.preventDefault();
+    e.preventDefault();
   }
 }, false);
 
@@ -164,25 +161,25 @@ let fracYValue = 0;
 let mouseXRange = mouseXEndPoint - mouseXStartPoint;
 
 const mouseMove = (event) => {
-    currentXPosition = event.clientX - mouseXStartPoint;
-    fracXValue = currentXPosition / mouseXRange;
+  currentXPosition = event.clientX - mouseXStartPoint;
+  fracXValue = currentXPosition / mouseXRange;
 
-    currentYPosition = event.clientY;
-    fracYValue = currentYPosition / mouseYEndPoint;
-    
-    let pupilXCurrrentPosition = pupilStartPoint + (fracXValue * pupilRange);
-    let pupilYCurrrentPosition = pupilStartPoint + (fracYValue * pupilRange);
+  currentYPosition = event.clientY;
+  fracYValue = currentYPosition / mouseYEndPoint;
 
-    pupilsArr.forEach((curPupil) => {
-        curPupil.style.transform= `translate(${pupilXCurrrentPosition}px, ${pupilYCurrrentPosition}px)`;
-    })
+  let pupilXCurrrentPosition = pupilStartPoint + (fracXValue * pupilRange);
+  let pupilYCurrrentPosition = pupilStartPoint + (fracYValue * pupilRange);
+
+  pupilsArr.forEach((curPupil) => {
+    curPupil.style.transform = `translate(${pupilXCurrrentPosition}px, ${pupilYCurrrentPosition}px)`;
+  })
 
 }
 
 const windowResize = (event) => {
-    mouseXEndPoint = window.innerWidth;
-    mouseYEndPoint = window.innerHeight;
-    mouseXRange = mouseXEndPoint - mouseXStartPoint;
+  mouseXEndPoint = window.innerWidth;
+  mouseYEndPoint = window.innerHeight;
+  mouseXRange = mouseXEndPoint - mouseXStartPoint;
 }
 
 
@@ -194,10 +191,20 @@ window.addEventListener('resize', windowResize);
 document.getElementById('resume-button-2').onclick = () => {
   // Delay the download event by 3 seconds
   window.open(
-        "https://drive.google.com/file/d/10jgXLbJ_7aYASdS1uTUJ2s2Q5z9bK689/view?usp=sharing"
-      ),
-  setTimeout(() => {
+    "https://drive.google.com/file/d/10jgXLbJ_7aYASdS1uTUJ2s2Q5z9bK689/view?usp=sharing"
+  ),
+    setTimeout(() => {
       // Trigger the download after the delay (replace 'resume-link-2' with your actual element ID)
       document.getElementById('resume-link-2').click();
-  }, 1000);
+    }, 1000);
 };
+// document.getElementById("Resume").onclick = () => {
+//   // Delay the download event by 3 seconds
+//   window.open(
+//     "https://drive.google.com/file/d/10jgXLbJ_7aYASdS1uTUJ2s2Q5z9bK689/view?usp=sharing"
+//   ),
+//     setTimeout(() => {
+//       // Trigger the download after the delay (replace 'resume-link-2' with your actual element ID)
+//       document.getElementById("Resume").click();
+//     });
+// };
